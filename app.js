487a1ea1020be1566fee7c96f1a7218dbf3ea6b2
Troy David Cook <www.troydavidcook@gmail.com>
2017-12-12 14:56:46 -0700

    Added navbar into header partial, more dummy data for dynamic columns.

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ var campgrounds = [
     { name: "Bents Camp", image: "http://bents-camp.com/2016/wp-content/uploads/2016/05/Bents-Camp-edited.jpg"},
     { name: "Milky Way Camp", image: "https://media.wired.com/photos/599b4cfd4fa6fc733c11e30d/master/pass/iStock-820873602.jpg"},
     { name: "Wilderness Camp", image: "http://www.wilderness-safaris.com/media/Camps/Botswana/Kwetsani%20Camp/10.CampGallery/kwetsani-galth.jpg"},
+    { name: "Big River", image: "http://www.diabetes.org/assets/img/community/camp/night-camp-fire.jpg"},
+    { name: "Bents Camp", image: "http://bents-camp.com/2016/wp-content/uploads/2016/05/Bents-Camp-edited.jpg"},
+    { name: "Milky Way Camp", image: "https://media.wired.com/photos/599b4cfd4fa6fc733c11e30d/master/pass/iStock-820873602.jpg"},
+    { name: "Wilderness Camp", image: "http://www.wilderness-safaris.com/media/Camps/Botswana/Kwetsani%20Camp/10.CampGallery/kwetsani-galth.jpg"},
+    { name: "Big River", image: "http://www.diabetes.org/assets/img/community/camp/night-camp-fire.jpg"},
+    { name: "Bents Camp", image: "http://bents-camp.com/2016/wp-content/uploads/2016/05/Bents-Camp-edited.jpg"},
+    { name: "Milky Way Camp", image: "https://media.wired.com/photos/599b4cfd4fa6fc733c11e30d/master/pass/iStock-820873602.jpg"},
+    { name: "Wilderness Camp", image: "http://www.wilderness-safaris.com/media/Camps/Botswana/Kwetsani%20Camp/10.CampGallery/kwetsani-galth.jpg"},
 ]
 
 // Beginning of RESTful routing
