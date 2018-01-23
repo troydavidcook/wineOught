3f0d5c95a594e59960e706462471c827ac34c267
Troy David Cook <www.troydavidcook@gmail.com>
2018-01-23 15:51:01 -0700

    change

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ const app = express();
 
 mongoose.Promise = global.Promise;
 
-// mongoose.connect('mongodb://troydavidcook:l0gical1@ds245287.mlab.com:45287/wine-ought', { useMongoClient: true });
-mongoose.connect('mongodb://localhost/wine_ought', { useMongoClient: true });
+mongoose.connect('mongodb://troydavidcook:l0gical1@ds245287.mlab.com:45287/wine-ought', { useMongoClient: true });
+// mongoose.connect('mongodb://localhost/wine_ought', { useMongoClient: true });
 
 // ===============
 // Passport Config
